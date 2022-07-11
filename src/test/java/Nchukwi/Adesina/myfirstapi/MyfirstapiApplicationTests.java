c249ebc9cd1611c9eba4c992f0b5d996647271af
Tamajong Nchukwi <tjnc@Tamajongs-MacBook-Pro-2.local>
2022-07-11 15:08:59 -0400

    api

---
+++ src/test/java/Nchukwi/Adesina/myfirstapi/MyfirstapiApplicationTests.java
@@ -0,0 +1,13 @@
+package Nchukwi.Adesina.myfirstapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
